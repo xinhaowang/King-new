ae083204000499c8f8f9e71e8e19466c4c5be83d
DONGFENG GU <gdf1992803@hotmail.com>
2014-02-20 11:50:17 -0500

    Event and Magic
    Event and Magic class in the project

---
+++ eventmagic.cpp
@@ -0,0 +1,5 @@
+#include "eventmagic.h"
+
+EventMagic::EventMagic()
+{
+}
---
+++ eventmagic.h
@@ -0,0 +1,10 @@
+#ifndef EVENTMAGIC_H
+#define EVENTMAGIC_H
+
+class EventMagic
+{
+public:
+    EventMagic();
+};
+
+#endif // EVENTMAGIC_H
