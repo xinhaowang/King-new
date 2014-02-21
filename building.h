66fefeee9bf29f702fa4f44830934fd1b5cf44f2
xinhaowang <xinhaowang@cmail.carleton.ca>
2014-02-20 19:30:49 -0500

    Update building.h
    I put building's constructor under public as it is not clear in the requirement.

--- building.h
+++ building.h
@@ -1,11 +1,29 @@
 #ifndef BUILDING_H
 #define BUILDING_H
 
+#include <QString>
+#include <vector>
+#include <QDebug>
+
+using namespace std;
+
+// Class title fot the player
+
 class Building
 {
 public:
-    Building();
+    //building's constructor
+    Building(int tempID, int tempIncome, QString tempUrl);
+    
+    // building ID accessors
+    int getID() const;
+    void setID(int value);
 
+private:
+    int ID;         //building ID
+    int income;     //building income
+    QString Url;    //building's Url
+    
 };
 
 #endif // BUILDING_H
